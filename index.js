3a52d8673cdd34f75959db7d332c06ead8fc8401
EvgeniyFedeykin <43994833+EvgeniyFedeykin@users.noreply.github.com>
2020-08-09 20:50:55 +0300

    1234

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 // JSON-файлы тоже можно подгружать через require!
 const config = require('./server/config.json');
 
+
 const express = require('express');
 const https = require('https');
 const http = require('http');
 const app = express();
 const cors = require('cors');
+const router = express.Router();
 
 const mysql = require("mysql2");
 
@@ -50,7 +52,7 @@ connection.connect(function(err){
 
 
 app.get('/login', (req, res) => {
-    const sql = `SELECT * FROM users where email = ${req.body.email} and password = ${req.body.password}`;
+    const sql = `SELECT * FROM users where email = "${req.body.email}" and password = "${req.body.password}"`;
     connection.query(sql,
         function(err, results, fields) {
             console.log(err);
@@ -66,26 +68,62 @@ app.get('/login', (req, res) => {
         connection.end();
 });
 
-app.post('/signup', (req, res) => {
+app.post('/signup', (req, res, next) => {
+    try {
     console.log("сработало");
     console.log(req.body);
     console.log(req.body.email);
     console.log(req.body.password);
-    const sql = `INSERT INTO users values(1, ${req.body.email}, ${req.body.password})`;
-    console.log(sql);
-    connection.query(sql,
+    let id;
+    connection.connect();
+    connection.query(`select count(*) AS userscount from todolistserver.users`,
         function(err, results, fields) {
-            console.log(err);
-            console.log(results); // собственно данные
-            console.log(fields); // мета-данные полей
+            console.log("ошибки" + err);
+            console.log("данные" + results); // собственно данные
+            //console.log(fields); // мета-данные полей
             if(!_.isEmpty(err)) {
-                res.status(500).send("Login failed");
+                throw err;    
+            }
+            //if(!_.isEmpty(results)) {
+                console.log("результаты " + results[0].userscount);
+                id = (results[0].userscount + 1);
+                console.log("ид " + id);
+                res.locals.id = id;
+                next();
+           // } 
+        });
+   } catch(e) {
+        console.log("ошибка" + e);
+        res.status(500).send(e);
+    }
+}, (req, res) => {
+    //try {
+        console.log("ид второе" + res.locals.id);
+        console.log(req.body);
+        console.log(req.body.email);
+        console.log(req.body.password);
+
+        const sql = `INSERT INTO todolistserver.users (idusers, email, password) VALUES (${res.locals.id}, "${req.body.email}", "${req.body.password}")`;
+        console.log("запрос" + sql);
+
+        connection.connect();
+        connection.query(sql,
+        function(err, results, fields) {
+            console.log("ошибки" + err);
+            console.log("данные" + results); // собственно данные
+            //console.log(fields); // мета-данные полей
+            if(!_.isEmpty(err)) {
+                throw err;
             }
             if(!_.isEmpty(results)) {
                 res.status(200).send(results);
             } 
         });
-        connection.end();
+        //connection.end();
+    //} catch(e) {
+    //    console.log("ошибка" + e);
+    //    res.status(500).send(e);
+    //}
 });
 
 app.listen(config.port, (err) => {
